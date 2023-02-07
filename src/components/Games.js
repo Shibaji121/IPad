0a5aa032c5978262fd41f0b27cda23077565e539
shiba_1234 <shibajisahu121@gmail.com>
2023-02-07 23:06:51 +0530

    games page pic changed

--- src/components/Games.js
+++ src/components/Games.js
@@ -8,7 +8,7 @@ export default function Games() {
         <div className="other-menu-style">
             <div>
             <img style={{width: '8.5em', height: '7em'}}  
-            src="https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/001/431/133/datas/original.jpg" alt="dice"></img>
+            src="https://previews.123rf.com/images/chrisdorney/chrisdorney1701/chrisdorney170100294/70009733-two-red-dice-isolated-over-a-plain-white-background-.jpg" alt="dice"></img>
             </div>
             Games
         </div>
